d0734e38690ef4d45121129c6fe1fb1fec976a11
Maksim Baturin <baturinmaksim188@gmail.com>
2022-10-06 19:05:58 +0600

    fix

--- src/Controller/DevController.php
+++ src/Controller/DevController.php
@@ -11,29 +11,25 @@ use Doctrine\ORM\EntityManagerInterface;
 class DevController extends AbstractController
 {
     /**
-     * @Route("/111", name="save")
+     * @Route("/add", name="save")
      */
     public function funcSave(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
 
         $product = new Developer();
-        $product->setName('Keyboard');
-        $product->setPosition('1999');
-        $product->setSkills('Ergonomic and stylish!');
+        $product->setName('Максим');
+        $product->setPosition('Джуниор');
+        $product->setSkills('Чистый код');
 
-        // сообщите Doctrine, что вы хотите (в итоге) сохранить Продукт (пока без запросов)
         $entityManager->persist($product);
-
-        // действительно выполните запросы (например, запрос INSERT)
         $entityManager->flush();
 
-        return new Response('Saved new product with id '.$product->getId());
-        return $this->render('dev/conduct.html.twig');
+        return new Response('Создана новая запись №'.$product->getId());
     }
 
     /**
-     * @Route("/222", name="out")
+     * @Route("/get", name="out")
      */
     public function funcOut(): Response
     {
